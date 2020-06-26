0bc1fe4e809ea95b7314efbd7e4da12a1b2790eb
peaky76 <robertjamespeacock@gmail.com>
2020-06-26 16:38:58 +0100

    guest: test fav song passed

--- guest.rb
+++ guest.rb
@@ -1,10 +1,11 @@
 class Guest
 
-    attr_reader :name, :cash
+    attr_reader :name, :cash, :fav_song
 
-    def initialize(name, cash)
+    def initialize(name, cash, fav_song)
         @name = name
         @cash = cash
+        @fav_song = fav_song
     end
 
     def pay(amount)
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -9,10 +9,10 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
 class TestGuest < MiniTest::Test
 
     def setup()
-        @guest_1 = Guest.new("Ali", 20)
-        @guest_2 = Guest.new("Bobby", 30)
-        @guest_3 = Guest.new("Charlie", 40)
         @song_1 = Song.new("Blur", "Song 2", "Wooo-hooo!")
+        @guest_1 = Guest.new("Ali", 20, @song_1)
+        @guest_2 = Guest.new("Bobby", 30, @song_1)
+        @guest_3 = Guest.new("Charlie", 40, @song_1)
     end
 
     def test_guest_has_name()
@@ -21,6 +21,10 @@ class TestGuest < MiniTest::Test
 
     def test_guest_has_cash()
         assert_equal(30, @guest_2.cash)
+    end
+
+    def test_guest_has_fav_song()
+        assert_equal("Blur", @guest_1.fav_song.artist)
     end
 
     def test_guest_pay()
@@ -36,10 +40,10 @@ class TestGuest < MiniTest::Test
         refute(@guest_1.can_pay?(20.01))
     end
 
-    def test_guest_pay_not_possible_if_can_pay_false()
+    def test_guest_pay_not_possible_if_guest_can_pay_false()
         # Guest tries to pay for something that they can't afford
         @guest_3.pay(50)
-        # Cash not taken so remains same
+        # Cash not taken
         assert(40, @guest_3.cash)
     end
 
