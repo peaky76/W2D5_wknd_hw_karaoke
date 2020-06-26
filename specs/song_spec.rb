2e19a1706e1a91971d7f03664a7bc1bec7fe23d1
peaky76 <robertjamespeacock@gmail.com>
2020-06-26 12:56:16 +0100

    song: test has title and artist passed

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -7,5 +7,17 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
 
 class TestSong < MiniTest::Test
 
+    def setup()
+        @song_1 = Song.new("Blur", "Song 2")
+        @song_2 = Song.new("U2", "One")
+    end
+
+    def test_has_artist()
+        assert_equal("Blur", @song_1.artist)
+    end
+
+    def test_has_title()
+        assert_equal("One", @song_2.title)
+    end
 
 end
