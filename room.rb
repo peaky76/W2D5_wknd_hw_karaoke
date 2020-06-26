d22549fe80510fa06da004d010e5f8ac1765c83b
peaky76 <robertjamespeacock@gmail.com>
2020-06-26 13:02:49 +0100

    room: test has name passed

--- room.rb
+++ room.rb
@@ -1,3 +1,9 @@
 class Room
 
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -7,5 +7,14 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
 
 class TestRoom < MiniTest::Test
 
+    def setup()
+        @room_1 = Room.new("Voodoo")
+        @room_2 = Room.new("Java")
+        @room_3 = Room.new("Africa")
+    end
+
+    def test_room_has_name()
+        assert_equal("Voodoo", @room_1.name)
+    end
 
 end
